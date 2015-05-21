1144f32b6f28e0f711932a21752d9c1912e1079f
t.hoefler <t.hoefler@141.19.145.83>
2015-05-21 17:08:40 +0200

    Endlich gerafft,Test ?

--- DB2/src/DB2Middle.java
+++ DB2/src/DB2Middle.java
@@ -6,165 +6,104 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-
 import javax.sql.rowset.CachedRowSet;
 
-
 public class DB2Middle {
-		private static Connection con = null;
-		private static String dbHost = "codd.2clever4you.net"; // Hostname
-		private static String dbPort = "3306";      // Port -- Standard: 3306
-		private static String dbName = "db212";   // Datenbankname
-		private static String dbUser = "db212";     // Datenbankuser
-		private static String dbPass = "arparili";      // Datenbankpasswort
-		private static ArrayList list = new ArrayList();
-		private static Statement query;
-		private DB2Middle(){
-	    try {
-	        Class.forName("com.mysql.jdbc.Driver"); // Datenbanktreiber für JDBC Schnittstellen laden.
-	 
-	        // Verbindung zur JDBC-Datenbank herstellen.
-	        con = DriverManager.getConnection("jdbc:mysql://"+dbHost+":"+ dbPort+"/"+dbName+"?"+"user="+dbUser+"&"+"password="+dbPass);
-	    } catch (ClassNotFoundException e) {
-	        System.out.println("Treiber nicht gefunden");
-	    } catch (SQLException e) {
-	        System.out.println("Verbindung nicht moglich");
-	        System.out.println("SQLException: " + e.getMessage());
-	        System.out.println("SQLState: " + e.getSQLState());
-	        System.out.println("VendorError: " + e.getErrorCode());
-	    }
-	  }
-	 
+	private static Connection con = null;
+	private static String dbHost = "codd.2clever4you.net"; // Hostname
+	private static String dbPort = "3306"; // Port -- Standard: 3306
+	private static String dbName = "db212"; // Datenbankname
+	private static String dbUser = "db212"; // Datenbankuser
+	private static String dbPass = "arparili"; // Datenbankpasswort
+	private static ArrayList list = new ArrayList();
+	private static Statement query;
 
-	private static Connection getConnection() throws SQLException{
-	    if(con == null)
-	        new DB2Middle();
-	    return con;
+	private DB2Middle() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver"); // Datenbanktreiber für JDBC
+													// Schnittstellen laden.
+
+			// Verbindung zur JDBC-Datenbank herstellen.
+			con = DriverManager.getConnection("jdbc:mysql://" + dbHost + ":"
+					+ dbPort + "/" + dbName + "?" + "user=" + dbUser + "&"
+					+ "password=" + dbPass);
+		} catch (ClassNotFoundException e) {
+			System.out.println("Treiber nicht gefunden");
+		} catch (SQLException e) {
+			System.out.println("Verbindung nicht moglich");
+			System.out.println("SQLException: " + e.getMessage());
+			System.out.println("SQLState: " + e.getSQLState());
+			System.out.println("VendorError: " + e.getErrorCode());
+		}
 	}
-	
-	/* public static void printNameList() {
-	       try { con = getConnection();
-	 
-	      if(con != null){
-	      // Abfrage-Statement erzeugen.
-	      Statement query;
-	    
-	          query = con.createStatement();
-	 
-	          // Tabelle anzeigen
-	          String sql ="SELECT RaumNr,Sitzplätze FROM Raum";
-	          ResultSet result = query.executeQuery(sql);
-	 
-	        // Ergebnisstabelle durchforsten
-	          while (result.next()) {
-	          String RaumNr = result.getString("RaumNr");
-	          String Sitzplätze = result.getString("Sitzplätze");
-	                
-	          String info = RaumNr+ ", " + Sitzplätze ;
-	         
-	          System.out.println(info);
-	          }
-	      return;}} catch (SQLException e) {
-	        e.printStackTrace();
-	      }
-	    }*/
-	  
-	
+
+	private static Connection getConnection() throws SQLException {
+		if (con == null)
+			new DB2Middle();
+		return con;
+	}
+
+	/*
+	 * public static void printNameList() { try { con = getConnection();
+	 * 
+	 * if(con != null){ // Abfrage-Statement erzeugen. Statement query;
+	 * 
+	 * query = con.createStatement();
+	 * 
+	 * // Tabelle anzeigen String sql ="SELECT RaumNr,Sitzplätze FROM Raum";
+	 * ResultSet result = query.executeQuery(sql);
+	 * 
+	 * // Ergebnisstabelle durchforsten while (result.next()) { String RaumNr =
+	 * result.getString("RaumNr"); String Sitzplätze =
+	 * result.getString("Sitzplätze");
+	 * 
+	 * String info = RaumNr+ ", " + Sitzplätze ;
+	 * 
+	 * System.out.println(info); } return;}} catch (SQLException e) {
+	 * e.printStackTrace(); } }
+	 */
 
 	public boolean createRoom(int seats) {
-		try { con = getConnection();
-	       
-	      if(con != null){
-	      // Abfrage-Statement erzeugen.
-	     
-	          query = con.createStatement();
-	 
-	          // Tabelle anzeigen
-	          String sql ="SELECT raumnr,sitzplätze FROM raum";
-	          ResultSet result = query.executeQuery(sql);
-	
-	        // Ergebnisstabelle durchforsten
-	          while (result.next()) {
-	        	
-	          String raumnr = result.getString("raumnr");
-	          String sitzplätze = result.getString("sitzplätze");
-	          
-	          String info = raumnr+ ", " + sitzplätze ;
-		         
-	          System.out.println(info);
-	          }
-	          }
-	      } catch (SQLException e) {
-	        e.printStackTrace();
-	      }
 		return false;
-	    }
+	}
 
-
-	
 	public static ResultSet getAllRooms() {
-			
-		         
-		       try { con = getConnection();
-		       
-		      if(con != null){
-		      // Abfrage-Statement erzeugen.
-		     
-		          query = con.createStatement();
-		 
-		          // Tabelle anzeigen
-		          String sql ="SELECT raumnr,sitzplätze FROM raum";
-		           ResultSet result = query.executeQuery(sql);
-		
-		        // Ergebnisstabelle durchforsten
-		          while (result.next()) {
-		        	
-		          String raumnr = result.getString("raumnr");
-		          String sitzplätze = result.getString("sitzplätze");
-		          
-		          String info = raumnr+ ", " + sitzplätze ;
-			         
-		          System.out.println(info);
-		          }
-		          }
-		      } catch (SQLException e) {
-		        e.printStackTrace();
-		      }
-			return null;
-		
-		    }
-	
-	/*  public static ResultSet getAllRooms() throws SQLException {
-	        String query = "SELECT * FROM employee";
-	        List<?> list = new ArrayList<>();
-	        String employee = null;
-	        ResultSet rs = null;
-	        try {
-	            con =getConnection();
-	            query = con.createStatement();
-	            rs = statement.executeQuery(query);
-	            while (rs.next()) {
-	                employee = new Employee();
-	              
-	                employee.setEmpId(rs.getInt("emp_id"));
-	                employee.setEmpName(rs.getString("emp_name"));
-	                employee.setDob(rs.getDate("dob"));
-	                employee.setSalary(rs.getDouble("salary"));
-	                employee.setDeptId((rs.getInt("dept_id")));
-	 
-	                //add each employee to the list
-	                list.add(employee);
-	            }
-	        } finally {
-	            DbUtil.close(rs);
-	            DbUtil.close(statement);
-	            DbUtil.close(connection);
-	        }
-	        return list;
-	    }*/
-	
-	
-		
+
+		try {
+			con = getConnection();
+
+			if (con != null) {
+				// Abfrage-Statement erzeugen.
+				query = con.createStatement();
+				// Tabelle anzeigen
+				String sql = "SELECT raumnr,sitzplätze FROM raum";
+				ResultSet result = query.executeQuery(sql);
+				return result;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+
+	}
+
+	/*
+	 * public static ResultSet getAllRooms() throws SQLException { String query
+	 * = "SELECT * FROM employee"; List<?> list = new ArrayList<>(); String
+	 * employee = null; ResultSet rs = null; try { con =getConnection(); query =
+	 * con.createStatement(); rs = statement.executeQuery(query); while
+	 * (rs.next()) { employee = new Employee();
+	 * 
+	 * employee.setEmpId(rs.getInt("emp_id"));
+	 * employee.setEmpName(rs.getString("emp_name"));
+	 * employee.setDob(rs.getDate("dob"));
+	 * employee.setSalary(rs.getDouble("salary"));
+	 * employee.setDeptId((rs.getInt("dept_id")));
+	 * 
+	 * //add each employee to the list list.add(employee); } } finally {
+	 * DbUtil.close(rs); DbUtil.close(statement); DbUtil.close(connection); }
+	 * return list; }
+	 */
 
 	public ResultSet getRoomById(int id) {
 
--- DB2/src/Test.java
+++ DB2/src/Test.java
@@ -1,9 +1,15 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 
 public class Test {
 
-	public static void main(String[] args) {
-		DB2Middle.getAllRooms();
-
+	public static void main(String[] args) throws SQLException {
+		ResultSet as = DB2Middle.getAllRooms();
+		
+		
+		while ( as.next() )
+			System.out.printf( "%s, %s, %s%n", as.getString(1), as.getString(2));
 	}
 
 }
